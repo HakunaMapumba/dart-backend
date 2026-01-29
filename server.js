f6f5fb80a03869749d23acd585994d64f83b93ba
rene <rene.ingenrieth@web.de>
2026-01-29 12:47:44 +0100

    Frontend eingebunden

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 
 const app = express();
+app.use(express.static("public"));
 app.use(express.json());
 
 // Test-Route
 app.get("/", (req, res) => {
-  res.json({ message: "Backend läuft!" });
+  res.sendFile("home.html", { root: "public" });
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.logt("Server lläuft auf Port", ))
 
 app.listen(3000, () => {
   console.log("Server läuft auf http://localhost:3000");
